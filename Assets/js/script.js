9c6e7b3de271925bba9bdba69dd17e2ae2a40fdb
Steven Nguyen <qviet715@gmail.com>
2023-01-29 16:24:53 -0500

    set up container for row blocks

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,33 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+$(document).ready(function() { 
+  var currentDate = dayjs().format('MMMM D, YYYY'); 
+  $("#current-date").text(currentDate);
+  // Code that interacts with the DOM goes here 
+  var currentHour = dayjs().hour(); 
+  $(".time-block").each(function() { 
+    var timeBlockId = $(this).attr("id"); 
+    if(timeBlockId < currentHour) { 
+      $(this).removeClass("present future").addClass("past"); 
+    } else if(timeBlockId == currentHour) { 
+      $(this).removeClass("past future").addClass("present"); 
+    } else { 
+      $(this).removeClass("past present").addClass("future"); 
+    } 
+    $(".time-block").each(function() { 
+      var timeBlockId = $(this).attr("id"); 
+      var savedInput = localStorage.getItem(timeBlockId); 
+      $(this).find("textarea").val(savedInput); 
+    }); 
+  
+  }); 
+
+  
+  }); 
+
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -8,6 +35,14 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  $("#save-button").click(function() { 
+    // Get the id of the containing time-block 
+    var timeBlockId = $(this).closest(".time-block").attr("id"); 
+    // Get the user input from the text field 
+    var userInput = $(this).closest(".time-block").find(".user-input").val(); 
+    // Save the user input in local storage using the time-block id as the key 
+    localStorage.setItem(timeBlockId, userInput); 
+  });
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
